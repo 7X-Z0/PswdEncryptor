2bff5d8b0f4031c9cc9fdef917f3334413a2c464
Lorenzo <Unix@system>
2025-03-23 13:34:59 -0400

    user input

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PswdEncryptor
-This is a rewrite to my old personal high-school project into rust
+This is a rewrite to my old personal high-school project into rust: 
+https://github.com/7X-Z0/PasswordEncrypter.git
 
 It is a test on how much programming I've learned throughout the years
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let _ = 
+
+    print!("Enter something: ");
+
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("you entered: {}", input.trim());
 }
