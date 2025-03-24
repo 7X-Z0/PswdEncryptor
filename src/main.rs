3f5c4928d7b1a0160a21b098d4e1b963a1cdc872
Lorenzo <Unix@system>
2025-03-24 10:41:30 -0400

    LET ME GENERATE OML

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io::{self, Write};
 use chacha20poly1305::{
-    aead::{Aead, AeadCore, KeyInit, OsRng},
-    ChaCha20Poly1305, Nonce
+    aead::{Aead, AeadCore, KeyInit, Nonce, OsRng},
+    ChaCha20Poly1305,
 };
-
+use rand::Rng;
 
 fn main() {
     let mut input = String::new();
-    let _ = 
+    let key: [u8; 32] = rand::rng().gen();
 
     print!("Enter something: ");
 
@@ -15,6 +15,5 @@ fn main() {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-
     
 }
