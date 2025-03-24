3d42ea02bc46bd183a583de1964685721a9fd685
Lorenzo <Unix@system>
2025-03-24 14:28:46 -0400

    added some notes

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use chacha20poly1305::{
 use base64::{engine::general_purpose, Engine};
 
 fn main() {
+    //user input
     let mut input = String::new();
+
+    //Generates key
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+
+    //Generates cipher using the key generated. Key changes everytime program is ran
     let cipher = ChaCha20Poly1305::new(&key);
+
+    //Generates nonce. Makes sure that the same input (plaintext) encrypted with the same key doesn’t produce the same output
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
 
     print!("Enter something: ");
 
+    //io = input output. idk what flush and unwrap means
     io::stdout().flush().unwrap();
     io::stdin()
         .read_line(&mut input)
